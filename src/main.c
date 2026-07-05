f1be0e67e801221598036b1c49c52de3c1a82661
HoJLter <hojlter.work@gmail.com>
2026-07-05 16:30:40 +0000

    Add: probe, remove, init, exit funcs

--- src/main.c
+++ src/main.c
@@ -1,32 +1,58 @@
 #include <linux/pci.h>
 #include <linux/module.h>
 
+#define DRIVER_NAME "Kintex-7 Driver"
 #define VENDOR_ID 0x10EE 
 #define DEVICE_ID 0x1234 
-#define SUBVENDOR_ID 0x10EE
-#define SUBDEVICE_ID 0x5678
 
 
 
-struct pci_device_id fpgaPciIdTable{
-    .vendor = VENDOR_ID,
-    .device = DEVICE_ID,
-    .subvendor = SUBVENDOR_ID,
-    .subdevice = SUBDEVICE_ID
-}
-
-struct pci_driver fpgaPciDriver = {
-    .name = "Kintex-7 Driver",
-    .id_table = &fpgaPciIdTable,
-    .probe = fpgaProbe,
-    remove = fpgaRemove 
-};
 
 
-int fpgaProbe(struct pci_dev* device){
+int fpgaProbe(struct pci_dev* device, const struct pci_device_id *ent){
     int err = pci_enable_device(device);
     if (err){
+        printk("[FPGA] ERROR WHILE ENABLING DEVICE OCCURED: %d", err);
         return err;
     }
-    printk("wassup");
+    err = pci_request_region(device, 0, DRIVER_NAME);
+    if (err){
+        printk("[FPGA] ERROR WHILE REQUESTING MEM REGION OCCURED: %d", err);
+        pci_disable_device(device);
+        return err;
+    }
+    
+    printk("[FPGA] DEVICE ENABLED AND BAR0 REQUESTED");
+
+    return 0;
 }
+
+void fpgaRemove(struct pci_dev* device){
+    printk("[FPGA] REMOVE FUNC CALLED");
+
+    pci_release_region(device, 0);
+    pci_disable_device(device);
+}
+
+struct pci_device_id fpgaPciIdTable[] = {
+    {PCI_DEVICE(VENDOR_ID, DEVICE_ID)},
+    {0}
+};
+
+struct pci_driver fpgaPciDriver = {
+    .name = DRIVER_NAME,
+    .id_table = fpgaPciIdTable,
+    .probe = fpgaProbe,
+    .remove = fpgaRemove 
+};
+
+int __init fpga_init(){
+    return pci_register_driver(&fpgaPciDriver);
+}
+
+void __exit fpga_exit(){
+    pci_unregister_driver(&fpgaPciDriver);
+}
+
+module_init(fpga_init);
+module_exit(fpga_exit);
