ba047e33a4bd0dd0376bb85570f6d5879c676d2d
HoJLter <hojlter.work@gmail.com>
2026-08-06 22:58:05 +0300

    Remove: IRQ

--- src/fpga_pcie.c
+++ src/fpga_pcie.c
@@ -17,18 +17,6 @@ struct drv_data {
     struct pci_dev* device; 
 };
 
-static irqreturn_t turn_leds_handler(int irq, void* dev){
-    printk("[FPGA] irq detected: turn_leds");
-    struct drv_data* data = dev;
-    u32 is_turned_on = readl((data -> bar0) + LEDS_REG);
-    printk("[FPGA] data read: %d", is_turned_on);
-
-    if (is_turned_on) writel(0, (data -> bar0) + LEDS_REG);
-    else writel(1, (data -> bar0) + LEDS_REG);
-
-    return IRQ_HANDLED;
-}
-
 static int fpga_probe(struct pci_dev* device, const struct pci_device_id *ent){
     printk("[FPGA] bar0 size = %llu\n", (unsigned long long)pci_resource_len(device, 0));
 
@@ -66,35 +54,6 @@ static int fpga_probe(struct pci_dev* device, const struct pci_device_id *ent){
     else{
         printk("[FPGA] mapping completed successfully");
     }
-
-    int alloc_vec_cnt = pci_alloc_irq_vectors(device, VEC_CNT, VEC_CNT, PCI_IRQ_MSI);
-    if (alloc_vec_cnt < 0){
-        printk("[FPGA] error while allocating irq vectors");
-        err = alloc_vec_cnt;
-        goto err_alloc_vec;
-    }
-    else{
-        printk("[FPGA] irq vector allocation completed successfully");
-    }
-
-    int turn_leds_vec = pci_irq_vector(device, 0);
-    if (turn_leds_vec < 0) {
-        printk("[FPGA] error while getting irq vector: %d", turn_leds_vec);
-        err = turn_leds_vec;
-        goto err_irq_request;
-    }
-    else{
-        printk("[FPGA] irq vector allocated: turn leds - %d", turn_leds_vec);
-    }
-
-    err = request_irq(turn_leds_vec, turn_leds_handler, 0, "FPGA leds on", data);
-    if (err){
-        printk("[FPGA] error while requesting irq");
-        goto err_irq_request;
-    }
-    else{
-        printk("[FPGA] irq registered successfully");
-    }
     
     if (data->bar0){
         writel(0b10101010110101010110101010110101, data -> bar0);
@@ -104,14 +63,6 @@ static int fpga_probe(struct pci_dev* device, const struct pci_device_id *ent){
     }  
 
     return 0;
-    
-    err_irq_request:
-        pci_free_irq_vectors(device);
-        printk("[FPGA] irq vectors freed");
-    
-    err_alloc_vec:
-        pci_iounmap(device, data->bar0);
-        printk("[FPGA] bar0 unmapped");
     
     err_mapping:
         pci_release_region(device, 0);
@@ -135,9 +86,6 @@ static void fpga_remove(struct pci_dev* device){
 
     struct drv_data* data = pci_get_drvdata(device);
     
-    free_irq(pci_irq_vector(device, 0), data);
-
-    pci_free_irq_vectors(device);
 
     pci_iounmap(device, data->bar0);
     pci_release_region(device, 0);
